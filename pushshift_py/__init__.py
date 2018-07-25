54ee2eb763a7224fbfec0e5c36be0ae081cfad0d
typenil <code@typenil.com>
2018-07-25 15:05:20 -0700

    Bump version

--- pushshift_py/__init__.py
+++ pushshift_py/__init__.py
@@ -7,4 +7,4 @@ https://github.com/typenil/pushshift.py
 from pushshift_py.src.pushshift_api_minimal import PushshiftAPIMinimal
 from pushshift_py.src.pushshift_api import PushshiftAPI
 
-__version__ = "0.0.5"
+__version__ = "0.0.6"
