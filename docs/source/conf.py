f05320395de76aab81c0c2602aac4c1de64b25ac
David Marx <david.marx84@gmail.com>
2020-03-14 20:41:20 -0700

    Attempt to fix 'contents.rst not found' bug

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,3 +53,8 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Bug fix for 
+#     "master file /home/docs/checkouts/readthedocs.org/user_builds/hoodie-test/checkouts/latest/contents.rst not found"
+# https://github.com/readthedocs/readthedocs.org/issues/2569
+master_doc = 'index'
